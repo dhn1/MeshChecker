3a18345d508ff2558fea2c2fedf740f66bbd5e13
Nethers <david_netherwood@hotmail.com>
2026-02-10 12:14:48 +0000

    .

--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -343,10 +343,10 @@ REGISTER_TEST (useCase03)
 
 static TestRes useCase04 ()
 {
-    auto target =
-        Triangle::create (Vertex::create (-6.415164999259999945, 5.6080837660800000322, -18), Vertex::create (-6.1588704557899998093, 5.8913302244899998783, -18), Vertex::create (-6.214446746370000163, 5.8357539339200004136, -18), "TT70491", 64);
-    auto other = Triangle::create (
-        Vertex::create (-6.2988778737000004071, 6.7076236509099995686, -17.918788480900001758), Vertex::create (-6.214446746370000163, 5.8357539339200004136, -18), Vertex::create (-6.0503716433100001026, 5.9998290369799995858, -18), "TT45273", 64);
+    auto target = Triangle::create (
+        Vertex::create (-6.415164999259999945, 5.6080837660800000322, -18), Vertex::create (-6.1588704557899998093, 5.8913302244899998783, -18), Vertex::create (-6.214446746370000163, 5.8357539339200004136, -18), QStringLiteral ("TT70491"), 64);
+    auto other = Triangle::create (Vertex::create (-6.2988778737000004071, 6.7076236509099995686, -17.918788480900001758), Vertex::create (-6.214446746370000163, 5.8357539339200004136, -18),
+        Vertex::create (-6.0503716433100001026, 5.9998290369799995858, -18), QStringLiteral ("TT45273"), 64);
 
     auto vp = VertexPool::create ();
     vp->update (target);
@@ -369,9 +369,9 @@ REGISTER_TEST (useCase04)
 static TestRes useCase05 ()
 {
     auto target = Triangle::create (Vertex::create (34.983099559400002931, 2.2009500682800000604, -9.635073482030000136), Vertex::create (35.569933053799999811, 2.2378705023099998428, -9.0798099947900006157),
-        Vertex::create (35.640260967499997946, 2.182336575730000151e-15, -9.0798099947900006157), "TT12949", 64);
+        Vertex::create (35.640260967499997946, 2.182336575730000151e-15, -9.0798099947900006157), QStringLiteral ("TT12949"), 64);
     auto other = Triangle::create (Vertex::create (35.052267201799999441, 2.1463323415699999308e-15, -9.635073482030000136), Vertex::create (35.640260967499997946, 2.182336575730000151e-15, -9.0798099947900006157),
-        Vertex::create (35.569933053799999811, -2.2378705023099998428, -9.0798099947900006157), "TT12951", 64);
+        Vertex::create (35.569933053799999811, -2.2378705023099998428, -9.0798099947900006157), QStringLiteral ("TT12951"), 64);
 
     auto vp = VertexPool::create ();
     vp->update (target);
@@ -394,9 +394,9 @@ REGISTER_TEST (useCase05)
 static TestRes useCase06 ()
 {
     auto target = Triangle::create (Vertex::create (18.797366735400000692, 1.1826300734099999268, -15.923727499600000002), Vertex::create (19.825591414200001594, 1.2139673533299999681e-15, -15.620066621799999496),
-        Vertex::create (19.786470138300000343, 1.2448591848699999129, -15.620066621799999496), "TT36149", 64);
+        Vertex::create (19.786470138300000343, 1.2448591848699999129, -15.620066621799999496), QStringLiteral ("TT36149"), 64);
     auto other = Triangle::create (Vertex::create (18.834532382799999084, 1.1532824897999999192e-15, -15.923727499600000002), Vertex::create (19.786470138300000343, -1.2448591848699999129, -15.620066621799999496),
-        Vertex::create (19.825591414200001594, 1.2139673533299999681e-15, -15.620066621799999496), "TT36151", 64);
+        Vertex::create (19.825591414200001594, 1.2139673533299999681e-15, -15.620066621799999496), QStringLiteral ("TT36151"), 64);
 
     auto vp = VertexPool::create ();
     vp->update (target);
@@ -419,9 +419,9 @@ REGISTER_TEST (useCase06)
 static TestRes useCase07 ()
 {
     auto target = Triangle::create (Vertex::create (17.42475366749999921, 0.017424753667500000875, -19.004668948700000897), Vertex::create (17.417528385899998966, 0.24733694153899998813, -19.066273733700001003),
-        Vertex::create (17.425301262400001434, 1.0669919707600000829e-15, -18), "TT43942", 1088);
-    auto other = Triangle::create (
-        Vertex::create (17.425301262400001434, 1.0669919707600000829e-15, -18), Vertex::create (17.412785682100000884, -0.39825222238099999839, -18), Vertex::create (17.425301262400001434, 4.4088823393800002323e-16, -19), "TT111919", 64);
+        Vertex::create (17.425301262400001434, 1.0669919707600000829e-15, -18), QStringLiteral ("TT43942"), 1088);
+    auto other = Triangle::create (Vertex::create (17.425301262400001434, 1.0669919707600000829e-15, -18), Vertex::create (17.412785682100000884, -0.39825222238099999839, -18), Vertex::create (17.425301262400001434, 4.4088823393800002323e-16, -19),
+        QStringLiteral ("TT111919"), 64);
 
     auto vp = VertexPool::create ();
     vp->update (target);
@@ -443,8 +443,10 @@ REGISTER_TEST (useCase07)
 
 static TestRes useCase08 ()
 {
-    auto target = Triangle::create (Vertex::create (44.075474737199996866, 79.067209756799996967, 2), Vertex::create (0.9442936788789999536, 15.640481640799999141, 2), Vertex::create (0.633628940779000005, 12.819595897199999257, 2), "TT262", 0);
-    auto other = Triangle::create (Vertex::create (0.9442936788789999536, 15.640481640799999141, 2), Vertex::create (0.633628940779000005, 12.819595897199999257, 39.5), Vertex::create (0.633628940779000005, 12.819595897199999257, 2), "TT159", 0);
+    auto target = Triangle::create (
+        Vertex::create (44.075474737199996866, 79.067209756799996967, 2), Vertex::create (0.9442936788789999536, 15.640481640799999141, 2), Vertex::create (0.633628940779000005, 12.819595897199999257, 2), QStringLiteral ("TT262"), 0);
+    auto other = Triangle::create (
+        Vertex::create (0.9442936788789999536, 15.640481640799999141, 2), Vertex::create (0.633628940779000005, 12.819595897199999257, 39.5), Vertex::create (0.633628940779000005, 12.819595897199999257, 2), QStringLiteral ("TT159"), 0);
 
     auto vp = VertexPool::create ();
     vp->update (target);
@@ -466,10 +468,10 @@ REGISTER_TEST (useCase08)
 
 static TestRes useCase09 ()
 {
-    auto target =
-        Triangle::create (Vertex::create (3.3512096405029296875, 7.8341774940490722656, -18), Vertex::create (3.8266692161560058594, 7.613307952880859375, -18), Vertex::create (3.7787721157073974609, 7.6377129554748535156, -18), "TT21370", 0);
-    auto other = Triangle::create (
-        Vertex::create (4.43286895751953125, 8.063358306884765625, -17.918788909912109375), Vertex::create (3.6297621726989746094, 7.7136373519897460938, -18), Vertex::create (3.8365087509155273438, 7.6082944869995117188, -18), "TT20013", 0);
+    auto target = Triangle::create (
+        Vertex::create (3.3512096405029296875, 7.8341774940490722656, -18), Vertex::create (3.8266692161560058594, 7.613307952880859375, -18), Vertex::create (3.7787721157073974609, 7.6377129554748535156, -18), QStringLiteral ("TT21370"), 0);
+    auto other = Triangle::create (Vertex::create (4.43286895751953125, 8.063358306884765625, -17.918788909912109375), Vertex::create (3.6297621726989746094, 7.7136373519897460938, -18),
+        Vertex::create (3.8365087509155273438, 7.6082944869995117188, -18), QStringLiteral ("TT20013"), 0);
 
     auto vp = VertexPool::create ();
     vp->update (target);
@@ -495,15 +497,15 @@ static TestRes sharedEdge01 ()
     auto v2 = Vertex::create (0, -100);
     auto v3 = Vertex::create (-100, 0);
     auto v4 = Vertex::create (100, 0);
-    v1->setAnnotation ("V1");
-    v2->setAnnotation ("V2");
-    v3->setAnnotation ("V3");
-    v4->setAnnotation ("V4");
+    v1->setAnnotation (QStringLiteral ("V1"));
+    v2->setAnnotation (QStringLiteral ("V2"));
+    v3->setAnnotation (QStringLiteral ("V3"));
+    v4->setAnnotation (QStringLiteral ("V4"));
 
     auto target = Triangle::create (v1, v4, v2);
-    target->setEdgeAnnotation (0, "E0");
-    target->setEdgeAnnotation (1, "E1");
-    target->setEdgeAnnotation (2, "E2");
+    target->setEdgeAnnotation (0, QStringLiteral ("E0"));
+    target->setEdgeAnnotation (1, QStringLiteral ("E1"));
+    target->setEdgeAnnotation (2, QStringLiteral ("E2"));
 
     auto other = Triangle::create (v1, v2, v3);
 
@@ -537,15 +539,15 @@ static TestRes sharedEdge02 ()
     auto v2 = Vertex::create (0, -100);
     auto v3 = Vertex::create (0, 0, -100);
     auto v4 = Vertex::create (100, 0);
-    v1->setAnnotation ("V1");
-    v2->setAnnotation ("V2");
-    v3->setAnnotation ("V3");
-    v4->setAnnotation ("V4");
+    v1->setAnnotation (QStringLiteral ("V1"));
+    v2->setAnnotation (QStringLiteral ("V2"));
+    v3->setAnnotation (QStringLiteral ("V3"));
+    v4->setAnnotation (QStringLiteral ("V4"));
 
     auto target = Triangle::create (v1, v4, v2);
-    target->setEdgeAnnotation (0, "E0");
-    target->setEdgeAnnotation (1, "E1");
-    target->setEdgeAnnotation (2, "E2");
+    target->setEdgeAnnotation (0, QStringLiteral ("E0"));
+    target->setEdgeAnnotation (1, QStringLiteral ("E1"));
+    target->setEdgeAnnotation (2, QStringLiteral ("E2"));
 
     auto other = Triangle::create (v1, v2, v3);
 
@@ -644,10 +646,11 @@ REGISTER_TEST (useCase10)
 
 static TestRes useCase11 ()
 {
-    auto t1 = Triangle::create (
-        Vertex::create (19.798989873200000034, 2.1107702762699998011, -19.798989873200000034), Vertex::create (21.288255852300000726, 100, -18.181888150599998966), Vertex::create (19.796096761200001168, 100, -19.796096761200001168), "t1", 0);
+    auto t1 = Triangle::create (Vertex::create (19.798989873200000034, 2.1107702762699998011, -19.798989873200000034), Vertex::create (21.288255852300000726, 100, -18.181888150599998966),
+        Vertex::create (19.796096761200001168, 100, -19.796096761200001168), QStringLiteral ("t1"), 0);
 
-    auto t2 = Triangle::create (Vertex::create (21.213203435600000546, 100, -21.213203435600000546), Vertex::create (18.181888150599998966, 100, -21.288255852300000726), Vertex::create (19.796096761200001168, 100, -19.796096761200001168), "t2", 0);
+    auto t2 = Triangle::create (
+        Vertex::create (21.213203435600000546, 100, -21.213203435600000546), Vertex::create (18.181888150599998966, 100, -21.288255852300000726), Vertex::create (19.796096761200001168, 100, -19.796096761200001168), QStringLiteral ("t2"), 0);
 
     auto vp = VertexPool::create ();
     vp->update (t1);
@@ -680,9 +683,9 @@ REGISTER_TEST (useCase11)
 
 static TestRes useCase12 ()
 {
-    auto t1 = Triangle::create (Vertex::create (5.0643400553599997593, 114.87999999999999545, 0.39857220629700002013), Vertex::create (4, 115, 0), Vertex::create (3.9876693349300000868, 115, 0.31383638291100002249), "", 0);
+    auto t1 = Triangle::create (Vertex::create (5.0643400553599997593, 114.87999999999999545, 0.39857220629700002013), Vertex::create (4, 115, 0), Vertex::create (3.9876693349300000868, 115, 0.31383638291100002249), QStringLiteral (""), 0);
 
-    auto t2 = Triangle::create (Vertex::create (5.0800000000000000711, 114.87999999999999545, 0), Vertex::create (3.9876693349300000868, 115, -0.31383638291100002249), Vertex::create (4, 115, 0), "", 0);
+    auto t2 = Triangle::create (Vertex::create (5.0800000000000000711, 114.87999999999999545, 0), Vertex::create (3.9876693349300000868, 115, -0.31383638291100002249), Vertex::create (4, 115, 0), QStringLiteral (""), 0);
 
     auto vp = VertexPool::create ();
     vp->update (t1);
@@ -711,7 +714,7 @@ static TestRes useCase12 ()
 }
 REGISTER_TEST (useCase12)
 
-static TestRes useCase13     ()
+static TestRes useCase13 ()
 {
     auto t1 = Triangle::create (Vertex::create (0, 40, 0), Vertex::create (19.999999999999996447, 34.641016151377549193, 40), Vertex::create (39.125904029352227553, -8.3164676327103652653, 40));
     auto t2 = Triangle::create (Vertex::create (-38.042260651806145688, 12.360679774997889169, 0), Vertex::create (-26.765224254354350819, 29.725793019095746672, 40), Vertex::create (19.999999999999996447, 34.641016151377549193, 40));
@@ -725,9 +728,9 @@ static TestRes useCase13     ()
     mesh->push_back (t1);
     mesh->push_back (t2);
 
-    auto segOfIntersection = t1->plane().intersection(t2->plane());
+    auto segOfIntersection = t1->plane ().intersection (t2->plane ());
     DocumentNethers doc (mesh);
-    doc.add(Segment::create(segOfIntersection));
+    doc.add (Segment::create (segOfIntersection));
     doc.write (outputFileName (gTestName, "nethers"));
 
     MeshChecker mc (mesh);
@@ -745,4 +748,27 @@ static TestRes useCase13     ()
     VERIFY (res.m_badCount == 0)
     return Passed;
 }
-REGISTER_TEST(useCase13)
+REGISTER_TEST (useCase13)
+
+static TestRes overlapTestCase01 ()
+{
+    auto v1 = Vertex::create (100, 16.428717105263153542, 6.5815460526315847645, QStringLiteral ("v1"));
+    auto v2 = Vertex::create (99.999999999999943157, 16.602552164169267712, 6.9090201190256115282, QStringLiteral ("v2"));
+
+    auto t1 = Triangle::create (v1, v2, Vertex::create (100, 16.60348684210525505, 6.9104605263157949935, QStringLiteral ("v3")), {}, 21568);
+    auto t2 = Triangle::create (v2, v1, Vertex::create (100, 20.439447368421042484, 10.532210526315795462, QStringLiteral ("v4")), {}, 17472);
+
+    auto mesh = Mesh::create ();
+    mesh->add (t1);
+    mesh->add (t2);
+
+    Document::write (mesh, outputFileName (gTestName, "nethers"));
+
+    MeshChecker mc (mesh);
+    auto res = mc.checkTriangleOverlap ();
+
+    VERIFY (res.m_badCount == 1)
+
+    return Passed;
+}
+REGISTER_TEST (overlapTestCase01)
